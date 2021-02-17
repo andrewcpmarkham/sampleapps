09c6aa50c4cae429ec9aec2c94fa10aa370fe1c0
Andrew Markham <andrew.markham@iinet.net.au>
2021-02-17 17:10:40 +1100

    Perfromance Test
    Added performance test for Awards

---
+++ UltimatePortfolio/UltimatePortfolioTests/PerformanceTests.swift
@@ -0,0 +1,32 @@
+//
+//  PerformanceTests.swift
+//  UltimatePortfolioTests
+//
+//  Created by Andrew CP Markham on 17/2/21.
+//
+
+import XCTest
+@testable import UltimatePortfolio
+
+class PerformanceTests: BaseTestCase {
+    func testAwardCalculationPerformance() throws {
+        // Create a significant amount of test data
+        for _ in 1...100 {
+            try dataController.createSampleData()
+        }
+
+        // Simulate lots of awards to check
+        // Make one big single array
+        let awards = Array(repeating: Award.allAwards, count: 25).joined()
+        XCTAssertEqual(awards.count, 500, "This checks the awards count is constant. Change this if you add awards." )
+
+        measure {
+            _ = awards.filter(dataController.hasEarned)
+        }
+    }
+
+}
+// Notes for Paul
+// No mention of Name of File in notes
+// Second last code snipet doesn't compare to 1 as described
+// Mentions 4000 in the notes when it should be 500
